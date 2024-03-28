6bab3d4da48ec171db11902152f7f142eee0baf2
Nazar <Dumi_sj18@itstepru.onmicrosoft.com>
2024-03-28 17:36:03 +0500

    Some mail view changes

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -2,7 +2,7 @@
 {{-- Header --}}
 <x-slot:header>
 <x-mail::header :url="config('app.url')">
-{{ config('app.name') }}
+    <img src="https://blotos.ru/wp-content/uploads/f/b/d/fbdb60147c4e1e4b924f6b85991ae9e0.png" alt="App Logo" width="250px">
 </x-mail::header>
 </x-slot:header>
 
@@ -21,7 +21,7 @@
 {{-- Footer --}}
 <x-slot:footer>
 <x-mail::footer>
-© {{ date('Y') }} {{ config('app.name') }}. {{ __('All rights reserved.') }}
+© {{ date('Y') }} {{ config('app.name') }}. {{ __('ru.All rights reserved.') }}
 </x-mail::footer>
 </x-slot:footer>
 </x-mail::layout>
