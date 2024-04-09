3c77f2a1bc28b490bd0be5c047bc89a9abc8dc7d
Nazar <Dumi_sj18@itstepru.onmicrosoft.com>
2024-04-09 19:11:09 +0500

    Add mail DocUserCreated

---
+++ app/Mail/DocUserCreated.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use App\Models\Doctor;
+
+class DocUserCreated extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(public Doctor $doctor, public string $password)
+    {
+
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Регистрация на портале сотрудников Клиники Долголетия',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'custom.doc-user-added',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
--- routes/doc.php
+++ routes/doc.php
@@ -4,5 +4,6 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/panel', function () {
-    return view('docPanel.panel');
+    //return view('docPanel.panel');
+    return view('custom.doc-user-added');
 })->name('panel');
